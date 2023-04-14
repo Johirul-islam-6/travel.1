e71b0d088438be303bf7dd7c06112ffbdf52de9a
Zintu paul <jintupaul45@gmail.com>
2023-04-14 11:32:33 +0600

    desing contact section and review form add

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,12 @@
 
 import Adventures from "../../components/Adventure/Adventures";
+import Contact from "../../components/Contact/Contact";
+import FeedbackForm from "../../components/FeedbackForm/FeedbackForm";
 import Hero from "../../components/Home/Hero";
 import Meta from "../../components/Meta";
 import Testimonial from "../../components/Testimonial/Testimonial";
 import Services from "../../components/Testimonial/Testimonial";
+
 
 const Home = () => {
   return (
@@ -16,6 +19,8 @@ const Home = () => {
       <Hero />
       <Adventures />
       <Testimonial />
+      <Contact/>
+      <FeedbackForm/>
     </>
   );
 };
