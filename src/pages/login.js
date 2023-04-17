93d883a2c7bc85bbe0c30b24d2cdd51f4e0a100d
RidoyMia <Ridoymia153159hero@gmail.com>
2023-04-17 15:29:54 +0600

    firebase install done by me

--- src/pages/login.js
+++ src/pages/login.js
@@ -3,21 +3,23 @@ import styles from '../styles/mainService.module.css';
 import { BsFacebook } from "react-icons/bs";
 import { CiUser } from "react-icons/ci";
 import { AiOutlineGoogle, AiOutlineTwitter } from "react-icons/ai";
-import { useDispatch } from "react-redux";
+
 import { AUTHLOGIN } from "@/redux/slices/auth.slice";
+import Link from 'next/link';
+
 // import google from '../../components/Images/Google-Icon-PNG_rwscww-removebg-preview.png';
 // import  from '../../components/Images/Google-Icon-PNG_rwscww-removebg-preview.png';
 // import google from '../../components/Images/Google-Icon-PNG_rwscww-removebg-preview.png';
 
 const Login = () => {
-  const dispatch = useDispatch()
+
   return (
     <>
       <div className={styles.registerContainer}>
         <div className={styles.divission}>
           <div className={styles.LoginArea}>
 
-            <button className={styles.userIcon}>   < CiUser className={styles.RealIcon} /></button>
+            
             <h2 className="text-center font-semibold pt-3 text-xl">User login form</h2>
             <div className="text-left px-5">
               <form className="text-left">
@@ -25,7 +27,7 @@ const Login = () => {
                 <input className={styles.loginInput} type="email"></input>
                 <p className="text-left">Password :</p>
                 <input className={styles.loginInput} type="password"></input>
-                <button type='submit' onClick={(e) => { e.preventDefault(); dispatch(AUTHLOGIN()) }} className={styles.SubmitBtn}>login</button>
+                <button type='submit'  className={styles.SubmitBtn}>login</button>
               </form>
               <div className="text-center py-3">  <Link className="text-blue-700 align-center" href="/recovery" >Forget password?</Link></div>
             </div>
