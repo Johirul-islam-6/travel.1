bcf4d95c552fa62eb86cfa7589eda94970083523
minhaz000 <mrmminhaz@gmail.com>
2023-04-18 14:35:41 +0600

    commit from minhaz

--- hooks/hook.axios.js
+++ hooks/hook.axios.js
@@ -1,13 +1,7 @@
 import axios from "axios"
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-
 const errorArry = [401,403,406,500,400]
-=======
->>>>>>> b1266b54056505c9356ef99835c662d1203b11cd
-=======
->>>>>>> b1266b54056505c9356ef99835c662d1203b11cd
+
 const instance = axios.create({
   baseURL: 'http://localhost:5000',
   timeout: 1000,
@@ -19,7 +13,7 @@ instance.interceptors.response.use(response => {
 }, error => {
  if ( errorArry.includes(error.response.status)) {
   console.log(error)
-  // return Promise.reject()
+  return Promise.reject()
  }
  return Promise.reject(error);
 });
