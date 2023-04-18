8be0178814c3b4b043fb49f4b12c7402be8467d3
Jharna Amin <jharna3khatun@gmail.com>
2023-04-18 14:27:39 +0600

    added login and registration file

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,77 +1,8 @@
-<<<<<<< HEAD
-
-import styles from '../styles/mainService.module.css';
-import { BsFacebook } from "react-icons/bs";
-import { CiUser } from "react-icons/ci";
-import { AiOutlineGoogle, AiOutlineTwitter } from "react-icons/ai";
-import { useDispatch } from "react-redux";
-import { AUTHSIGNIN } from "@/redux/slices/auth.slice";
-import { useForm } from "react-hook-form";
-import { useAuthFetchQuery, useAuthLoginMutation } from "@/redux/api/auth.api";
-// import google from '../../components/Images/Google-Icon-PNG_rwscww-removebg-preview.png';
-// import  from '../../components/Images/Google-Icon-PNG_rwscww-removebg-preview.png';
-// import google from '../../components/Images/Google-Icon-PNG_rwscww-removebg-preview.png';
-=======
 import Link from "next/link";
 import { FaUserAlt } from "react-icons/fa";
 import classNames from "classnames";
->>>>>>> master
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-function login () {
-const dispatch = useDispatch()
-// const { data = [] , isError , isSuccess , isUninitialized} =  useAuthFetchQuery()
-const [ addlofgin , result ] = useAuthLoginMutation()
-const { register, handleSubmit, watch, formState: { errors } } = useForm();
-const onSubmit = data =>{
-  console.log(data);
-  // dispatch(AUTHSIGNIN(data)) 
-  addlofgin({mane:"minhaz"})
-
-}
-
-
-=======
-=======
->>>>>>> b1266b54056505c9356ef99835c662d1203b11cd
 const Login = () => {
-<<<<<<< HEAD
-  const dispatch = useDispatch()
->>>>>>> b1266b54056505c9356ef99835c662d1203b11cd
-  return (
-    <> { console.log( result ) }
-      <div className={styles.registerContainer}>
-        <div className={styles.divission}>
-<<<<<<< HEAD
-<<<<<<< HEAD
-           <div className={styles.LoginArea}>
-           
-               <button className={styles.userIcon}>   < CiUser className={styles.RealIcon} /></button>
-               <h2 className="text-center font-semibold pt-3 text-xl">User login form</h2>
-             <div className="text-left px-5">
-             <form className="text-left" onSubmit={handleSubmit(onSubmit)}>
-               <p className="text-left">Email :</p>
-               <input {...register('username',{ required: true })} className={styles.loginInput} ></input>
-               <p className="text-left">Password :</p>
-               <input {...register('password',{ required: true })} className={styles.loginInput} type="password"></input>
-               <button type='submit'  className={styles.SubmitBtn}>login</button>
-             </form>
-           <div className="text-center py-3">  <Link className="text-blue-700 align-center" href="/recovery" >Forget password?</Link></div>
-             </div>
-             <p>Dont  have an account? <Link href="/register" className="text-blue-700 ">Register</Link></p>
-              
-           </div>
-        </div>
-      </div>
-=======
-=======
->>>>>>> b1266b54056505c9356ef99835c662d1203b11cd
-          <div className={styles.LoginArea}>
-
-            <button className={styles.userIcon}>   < CiUser className={styles.RealIcon} /></button>
-            <h2 className="text-center font-semibold pt-3 text-xl">User login form</h2>
-=======
   const backgroundClass = classNames(
     "bg-[url('https://i.ibb.co/hRKMMLH/Rectangle.png')] max-h-screen bg-no-repeat bg-center bg-cover bg-fixed"
   );
@@ -88,7 +19,6 @@ const Login = () => {
           <div className={formClass}>
             <FaUserAlt className="absolute top-[-10%] left-[40%] text-7xl bg-[#627FF4] px-5 rounded-full" />
             <h1 className="text-center py-5 text-xl">Login </h1>
->>>>>>> master
             <div className="text-left px-5">
               <form className="text-left">
                 <div className="py-2">
@@ -132,18 +62,8 @@ const Login = () => {
               </p>
             </div>
           </div>
-<<<<<<< HEAD
-
-        </div >
-      </div >
-<<<<<<< HEAD
->>>>>>> b1266b54056505c9356ef99835c662d1203b11cd
-=======
->>>>>>> b1266b54056505c9356ef99835c662d1203b11cd
-=======
         </div>
       </div>
->>>>>>> master
     </>
   );
 };
