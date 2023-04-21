6f42d07e4d945ed080257a036d040b311984180d
minhaz000 <mrmminhaz@gmail.com>
2023-04-21 15:46:48 +0600

    axios confict solve

--- hooks/hook.axios.js
+++ hooks/hook.axios.js
@@ -1,10 +1,8 @@
 import axios from "axios";
 
-<<<<<<< HEAD
-const errorArry = [401,403,406,500,400]
-=======
+
 const errorArry = [401, 403, 406, 500, 400];
->>>>>>> 28a00d1562bf3fc6ed937639bdccb6d45d71b1e9
+
 
 const instance = axios.create({
   baseURL: "http://localhost:5000",
@@ -12,19 +10,7 @@ const instance = axios.create({
   withCredentials: true,
 });
 
-<<<<<<< HEAD
-instance.interceptors.response.use(response => {
-  return response;
-}, error => {
- if ( errorArry.includes(error.response.status)) {
-  console.log(error)
-  return Promise.reject()
- }
- return Promise.reject(error);
-});
 
-export default instance
-=======
 instance.interceptors.response.use(
   (response) => {
     return response;
@@ -37,6 +23,6 @@ instance.interceptors.response.use(
     return Promise.reject(error);
   }
 );
->>>>>>> 28a00d1562bf3fc6ed937639bdccb6d45d71b1e9
+
 
 export default instance;
