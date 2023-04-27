ee1e301331b9631d78061da68fd02d02e55b98ab
Jharna203 <jharnakhatun203@gmail.com>
2023-04-27 16:30:09 +0600

    added restaurant page

--- src/pages/destinationArea/[restaurants]/index.js
+++ src/pages/destinationArea/[restaurants]/index.js
@@ -1,18 +1,21 @@
 import { useEffect, useState } from "react";
-import Slider from "./Slider";
-import DestinationProduct from "./DestinationProduct";
+import Adventures from "../../../components/Adventure/Adventures";
+import Slider from "../../../components/Destinations/Slider";
+import { useRouter } from "next/router";
+import DestinationProduct from "../../../components/Destinations/DestinationProduct";
+import FeatureService from "../../../components/Home/Service/FeatureService/FeatureService";
 
-const Restaurants = () => {
+const Restaurants = ({ singelLocation }) => {
 
-  const [singelLocation, setLocation] = useState()
+  // const [singelLocation, setLocation] = useState()
   const id = '643c2b61f7c5a823c7ca6272'
 
-  useEffect(() => {
-    fetch(`https://travel-xone-server.vercel.app/api/v1/location/${id}`)
-      .then(res => res.json())
-      .then(data => setLocation(data))
+  // useEffect(() => {
+  //   fetch(`https://travel-xone-server.vercel.app/api/v1/location/${id}`)
+  //     .then(res => res.json())
+  //     .then(data => setLocation(data))
 
-  }, [])
+  // }, [])
 
 
 
@@ -25,3 +28,31 @@ const Restaurants = () => {
 };
 
 export default Restaurants;
+
+export const getStaticProps = async (context) => {
+  const id = context.params.destinations;
+  const res = await fetch(`https://travel-xone-server.vercel.app/api/v1/location/${id}`);
+  const data = await res.json();
+
+  return {
+    props: {
+      singelLocation: data,
+    }
+  }
+}
+
+export const getStaticPaths = async () => {
+  const res = await fetch("https://travel-xone-server.vercel.app/api/v1/locations/");
+  const data = await res.json();
+  const paths = data?.data?.map((currentLocation) => {
+    return {
+      params: {
+        destinations: currentLocation?._id.toString()
+      }
+    }
+  })
+  return {
+    paths,
+    fallback: false
+  }
+}
