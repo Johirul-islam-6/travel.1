d5dc540a8ecef6eab35ff92755c6b2bed77875a1
minhaz000 <mrmminhaz@gmail.com>
2023-04-30 20:05:00 +0600

    dynamic locartion

---
+++ src/context/RootContext.js
@@ -0,0 +1,39 @@
+import React, { createContext, useEffect, useState } from 'react';
+import axios from '../../hooks/hook.axios';
+const RootContext = createContext()
+function Context(props) {
+ const [status,setStatus] =useState({reload:false,loading:true})
+ const [user,setUser] = useState(null)
+ const LOGIN = (user)=>{
+          const url = '/auth/login'
+          return  axios.post(url,user)
+ }
+ const REGISTER = (newUser)=>{
+   console.log('newUser =>', newUser)
+        const url = '/auth/login'
+        return  axios.post(url,newUser)
+
+ }
+
+useEffect(()=>{
+        const unSubscribe = async()=>{
+                  console.log("fuck-up")
+                  const url = '/auth/profile'
+                  const profile =  await axios.get(url)
+                  setUser(profile.data.data)
+                  setStatus({...status,loading:false})
+
+      }
+      return ()=>  unSubscribe()
+},[])
+
+  const  RootContextValue = {status,setStatus,LOGIN,REGISTER,user,setUser}
+
+  return (
+  <RootContext.Provider value={RootContextValue}> 
+    {props.children}
+  </RootContext.Provider>
+  );
+}
+export {RootContext} 
+export default Context;
